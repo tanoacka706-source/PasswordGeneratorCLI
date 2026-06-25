f5a608ebbf678420b1d84051f8aebb6921109987
Acka Tano <tanoacka706@gmail.com>
2026-06-25 11:01:02 +0000

    Paramètre de longueur

--- src/main/java/ci/univmetiers/passwordgenerator/Main.java
+++ src/main/java/ci/univmetiers/passwordgenerator/Main.java
@@ -4,9 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PasswordGenerator generator = new PasswordGenerator();
+        int length = 20;
 
-        String password = generator.generate(12);
+        if (args.length > 0) {
+            try {
+                length = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Erreur : la longueur doit être un nombre entier.");
+                return;
+            }
+        }
+
+        System.out.println("Longueur utilisée : " + length);
+
+        PasswordGenerator generator = new PasswordGenerator();
+        String password = generator.generate(length);
 
         System.out.println("Mot de passe généré : " + password);
     }
